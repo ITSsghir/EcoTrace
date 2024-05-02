2c4a1e5077633df43d292357ac7f11f95c0c4c0c
Younest9 <y.elarjouni.sq@gmail.com>
2024-05-02 07:03:39 +0200

    Add utility functions for input validation

---
+++ back/utils.js
@@ -0,0 +1,116 @@
+// Description: This file contains the utility functions that are used in the back-end.
+
+// Function to initialize all necessary modules
+const initModules = (...modules) => {
+    for (const module of modules) {
+        module();
+    }
+}
+
+// Function to check if the email is authentic
+const checkEmailAuthenticity = async (email) => {
+
+    if (!email) {
+        return new Promise((resolve, reject) => {
+            reject(new Error('Email is required'));
+        });
+    }
+    // Check if the email is really an email
+    if (!email.includes('@')) {
+        return new Promise((resolve, reject) => {
+            reject(new Error('Invalid email'));
+        });
+    }
+    return new Promise((resolve, reject) => {
+        resolve(true);
+    });
+}
+
+// Function to check if the password is authentic
+const checkPasswordAuthenticity = async (password) => {
+
+    if (!password) {
+        return new Promise((resolve, reject) => {
+            reject(new Error('Password is required'));
+        });
+    }
+    // Check if the password is at least 8 characters long
+    if (password.length < 8) {
+        return new Promise((resolve, reject) => {
+            reject(new Error('Password must be at least 8 characters long'));
+        });
+    }
+    return new Promise((resolve, reject) => {
+        resolve(true);
+    });
+}
+
+// Function to check if the phone number is authentic
+const checkPhoneNumberAuthenticity = async (phone_number) => {
+
+    if (!phone_number) {
+        return new Promise((resolve, reject) => {
+            reject(new Error('Phone number is required'));
+        });
+    }
+    // Check if the phone number is a number
+    if (isNaN(phone_number)) {
+        return new Promise((resolve, reject) => {
+            reject(new Error('Phone number must be a number'));
+        });
+    }
+    if (!(phone_number.length === 10)) {
+        return new Promise((resolve, reject) => {
+            reject(new Error('Phone number must be 10 digits long'));
+        });
+    }
+    if (phone_number[0] !== '0') {
+        return new Promise((resolve, reject) => {
+            reject(new Error('Phone number must start with 0'));
+        });
+    }
+    return new Promise((resolve, reject) => {
+        resolve(true);
+    });
+}
+
+async function checkFullNameAuthenticity (full_name) {
+    if (!full_name) {
+        return new Promise((resolve, reject) => {
+            reject(new Error('Full name is required'));
+        });
+    }
+    // Must include a space between first name and last name
+    if (!full_name.includes(' ')) {
+        return new Promise((resolve, reject) => {
+            reject(new Error('Full name must include the first name and last name'));
+        });
+    }
+    return new Promise((resolve, reject) => {
+        resolve(true);
+    });
+}
+async function checkInputs (full_name, email, phone_number, password) {
+    
+    // Check if the full name is authentic
+    await checkFullNameAuthenticity(full_name);
+    
+    // Check if the email is authentic
+    await checkEmailAuthenticity(email);
+
+    // Check if the phone number is authentic
+    await checkPhoneNumberAuthenticity(phone_number);
+
+    // Check if the password is authentic
+    await checkPasswordAuthenticity(password);
+
+    return new Promise((resolve, reject) => {
+        resolve(true);
+    });
+}
+
+// Export the functions
+module.exports = {
+    initModules,
+    checkInputs,
+};
