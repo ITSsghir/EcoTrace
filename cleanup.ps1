3bd8320614229c562137bbbda96b853bef0458db
Younest9 <y.elarjouni.sq@gmail.com>
2024-05-05 04:39:09 +0200

    Update cleanup scripts to improve error handling and logging

--- cleanup.ps1
+++ cleanup.ps1
@@ -2,14 +2,14 @@
 ## It deletes the images and db data
 
 # Change directory to the backend folder
-Set-Location .\back 2>&1 $null && Write-Host "Changed the directory to the backend folder"
+Set-Location .\back 
 
 # Delete the images of the docker-compose
 # Get the images of the docker-compose
-docker image rm back-api 2>&1 $null && Write-Host "Deleted the back-api image"
+docker image rm back-api 
 
 # Delete db data
-Remove-Item -Recurse -Force .\db\ 2>&1 $null && Write-Host "Deleted the db data"
+Remove-Item -Recurse -Force .\db\
 
 # Change directory to the project root
-Set-Location .. 2>&1 $null && Write-Host "Changed the directory to the project root"
+Set-Location ..
