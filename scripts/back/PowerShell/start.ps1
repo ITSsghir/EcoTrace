019d6ee3027064eebbf7ab8ee439f21063f8a013
Younest9 <y.elarjouni.sq@gmail.com>
2024-05-05 21:27:18 +0200

    chore: Refactor PowerShell start script to improve backend setup
    This commit refactors the PowerShell start script to improve the backend setup process. It removes the installation of Docker Desktop, as it is assumed to be already installed. The script now focuses on changing the directory to the backend folder, which ensures that subsequent commands are executed in the correct context. This change simplifies the script and ensures that the backend services (db and express-api) can be started successfully.

--- scripts/back/PowerShell/start.ps1
+++ scripts/back/PowerShell/start.ps1
@@ -1,18 +1,6 @@
 ### This script is used to start the backend
 ## It installs the required packages and starts the backend services (db and express-api)
 
-# Install the required packages (docker desktop - includes docker cli and docker-compose) if not already installed
-if (-not (Test-Path "C:\Program Files\Docker\Docker\Docker Desktop.exe")) {
-    # Download the docker desktop installer
-    Invoke-WebRequest -Uri "https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe" -OutFile "C:\Docker Desktop Installer.exe"
-
-    # Install docker desktop
-    Start-Process -FilePath "C:\Docker Desktop Installer.exe" -Wait
-}
-
-# Start the docker desktop
-Start-Process -FilePath "C:\Program Files\Docker\Docker\Docker Desktop.exe" -Wait
-
 # Change the directory to the backend folder
 Set-Location .\back
 
