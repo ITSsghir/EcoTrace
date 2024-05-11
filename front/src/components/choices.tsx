abdd5988bfcc6500e5d7e1cc507d7ffff5fd25dc
Younest9 <y.elarjouni.sq@gmail.com>
2024-05-11 18:11:58 +0200

    refactor: Update Choices component to use ScrollView for horizontal scrolling

--- front/src/components/choices.tsx
+++ front/src/components/choices.tsx
@@ -1,4 +1,4 @@
-import { FlatList, TouchableOpacity, Image, View } from "react-native";
+import { FlatList, TouchableOpacity, Image, View, ScrollView } from "react-native";
 import React from "react";
 import icons from "@constants/icons";
 import { useRouter } from "expo-router";
@@ -8,29 +8,44 @@ export default function Choices() {
 
     const router = useRouter();
 
-    const choices = [
-        { id: '1', title: 'Camera', icon: icons.camera, route: 'Camera', onpress: () => router.push({ pathname: '/camera'})},
-        { id: '2', title: 'Microphone', icon: icons.microphone, route: 'Microphone', onpress: () => router.push({ pathname: '/microphone', params: { id: 1 } })},
-        { id: '3', title: 'Vehicle', icon: icons.car, route: 'Vehicle', onpress: () => router.push({ pathname: '/vehicle', params: { id: 1 } })},
-        { id: '4', title: 'Destination', icon: icons.destination, route: 'Destination', onpress: () => router.push({ pathname: '/destination', params: { id: 1 } })},
-    ];
-
-    const renderItem = ({ item }) => (
-        <TouchableOpacity style={styles.button} onPress={item.onpress}>
-            <Image source={item.icon} style={styles.icon} />
-        </TouchableOpacity>
-    );
     return (
-        <View>
-            <FlatList
-                data={choices}
-                keyExtractor={item => item.id}
-                renderItem={renderItem}
-                style={styles.choicesContainer}
-                showsVerticalScrollIndicator={false}
-                showsHorizontalScrollIndicator={false}
-                horizontal={true}
-            />
-        </View>
+        <ScrollView showsVerticalScrollIndicator={false} horizontal={true} style={styles.choicesContainer}>
+            <View>
+            <TouchableOpacity style={styles.button} onPress={
+                () => {
+                    router.push({ pathname: '/camera'})
+                }
+            }>
+                <Image source={icons.camera} style={styles.icon} />
+            </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity style={styles.button} onPress={
+                    () => {
+                        router.push({ pathname: '/microphone' })
+                    }
+                }>
+                    <Image source={icons.microphone} style={styles.icon} />
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity style={styles.button} onPress={
+                    () => {
+                        router.push({ pathname: '/vehicle' })
+                    }
+                }>
+                    <Image source={icons.car} style={styles.icon} />
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity style={styles.button} onPress={
+                    () => {
+                        router.push({ pathname: '/destination' })
+                    }
+                }>
+                    <Image source={icons.destination} style={styles.icon} />
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
     );
 }
